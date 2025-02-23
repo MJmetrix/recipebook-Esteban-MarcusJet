2cf7c3de5f53c1e46e87bd2cd3e17dad711f2838
Marcus Jet Esteban <marcusjetesteban@Marcuss-MacBook-Pro.local>
2025-02-23 15:50:23 +0800

    [recipebook] added codefor a basic homepage view

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-print("hi")
+def home(request):
+    return HttpResponse("Hi world")
