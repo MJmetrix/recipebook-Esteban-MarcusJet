381d1658dc1bdd28371106def2bac550e59e69a6
Marcus Jet Esteban <marcusjetesteban@Marcuss-MacBook-Pro.local>
2025-02-23 16:34:09 +0800

    [views, urls, html sites] updatedpaths with their corresponding html files

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, recipelist, foodrecipe
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', home, name="home"),
+    path("recipes/list/", recipelist, name="recipelist"),
+    path("recipe/<int:num>", foodrecipe, name="foodrecipe")
 ]
