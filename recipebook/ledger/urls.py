6c384ad7a8e0c465f1f8c6f21018a8b62f81caf7
Marcus Jet Esteban <marcusjetesteban@Marcuss-MacBook-Pro.local>
2025-03-20 11:03:21 +0800

    [urls] added a redirect option to the recipes/list/

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import Home, RecipeList, RecipeIngredientDatabase
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path("", lambda request: redirect("recipeList")),
     path("recipes/list/", RecipeList, name="recipeList"),
     path("recipe/<int:num>", RecipeIngredientDatabase, name="foodRecipe")
 ]
